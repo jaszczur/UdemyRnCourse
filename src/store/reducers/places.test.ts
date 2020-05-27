a23e9ea1517e2ec22ef0449d388c71f83a4d5523
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2020-05-27 12:23:36 +0200

    Add test for places reducer

--- src/store/reducers/places.test.ts
+++ src/store/reducers/places.test.ts
@@ -1,6 +1,6 @@
 import reducer from './places';
 import { INITIAL_STATE } from '../model';
-import { addPlace } from '../actions';
+import { addPlace, deletePlace } from '../actions';
 
 describe('Places reducer', () => {
   it('should return initial state', () => {
@@ -17,6 +17,19 @@ describe('Places reducer', () => {
     };
     expect(reducer(INITIAL_STATE.places, addPlace(place))).toEqual({
       places: [place],
+    });
+  });
+
+  it('should return state with place removed', () => {
+    const place = {
+      key: 'aaa',
+      name: 'bbb',
+      image: require('../../assets/amazing04.jpg'),
+    };
+    const state = reducer(INITIAL_STATE.places, addPlace(place));
+    expect(reducer(state, deletePlace(place.key))).toEqual({
+      places: [],
+      selectedPlace: null,
     });
   });
 });
